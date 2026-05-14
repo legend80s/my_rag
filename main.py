8f356a751068250e1dea5a554893a102f13fb508
legend80s <liuchzong@qq.com>
2026-05-14 17:19:21 +0800

    feat: switch to 本地文件 load

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 # https://docs.langchain.com/oss/python/langchain/rag#ollama
-import bs4
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 
 # from langchain_ollama import OllamaEmbeddings
 from langchain.tools import tool
-from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.embeddings import DeterministicFakeEmbedding
 
 # from langchain.chat_models import init_chat_model
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_deepseek import ChatDeepSeek
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import (
+    DirectoryLoader,
+    TextLoader,
+    # PyPDFLoader,
+    # Docx2txtLoader,
+)
+
+# 指定加载文档的目录
+LOAD_PATH = "./assets"
+
+# 作者：YuiGod
+# 链接：https://juejin.cn/post/7470807715898212406
+# 来源：稀土掘金
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 embeddings = DeterministicFakeEmbedding(size=4096)
 # embeddings = OllamaEmbeddings(model="llama3")
@@ -29,21 +41,35 @@ load_dotenv()
 
 # os.environ["OPENAI_API_KEY"] = API_KEY
 
+
 # Load and chunk contents of the blog
-loader = WebBaseLoader(
-    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
-    bs_kwargs=dict(
-        parse_only=bs4.SoupStrainer(
-            class_=("post-content", "post-title", "post-header")
-        )
-    ),
-)
-docs = loader.load()
+def load_docs(source_dir: str):
+    text_loader = DirectoryLoader(
+        source_dir,
+        glob=["**/*.txt", "**/*.md"],  # 指定读取文件的格式
+        # show_progress=True,  # 显示加载进度
+        # use_multithreading=True,  # 使用多线程
+        # silent_errors=True,  # 错误时不抛出异常，直接忽略该文件
+        loader_cls=TextLoader,  # 指定加载器
+        # fix UnicodeDecodeError: 'gbk' codec can't decode byte 0x9d in position 4900: illegal multibyte sequence
+        loader_kwargs={"encoding": "utf-8"},
+        # loader_kwargs={"autodetect_encoding": True},  # 自动检测文件编码
+    )
+    docs = text_loader.load()
+
+    return docs
+
+
+docs = load_docs(LOAD_PATH)
+print(f"加载了 {len(docs)} 个文档")
 
 assert len(docs) == 1
 
 print(f"Total characters: {len(docs[0].page_content)}")
+
+print("500 START")
 print(docs[0].page_content[:500])
+print("500 END")
 
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000,
